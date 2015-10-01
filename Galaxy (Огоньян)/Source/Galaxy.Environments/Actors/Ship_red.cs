e9747fd62c2edba85fda8554d66e759047c08c72
ZombieDust <shizuka_shinji@mail.ru>
2015-10-01 09:59:34 +0700

    Add Galaxy

---
+++ Galaxy (Огоньян)/Source/Galaxy.Environments/Actors/Ship_red.cs
@@ -0,0 +1,30 @@
+﻿#region using
+
+using System;
+using System.Diagnostics;
+using System.Windows;
+using Galaxy.Core.Actors;
+using Galaxy.Core.Environment;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
+
+#endregion
+
+namespace Galaxy.Environments.Actors
+{
+    internal class Ship_red : Ship
+    {
+        public Ship_red(ILevelInfo info):base(info)
+        {
+            Width = 25;
+            Height = 35;
+            ActorType = ActorType.Enemy;
+        }
+
+        public override void Load()
+        {
+            base.Load();
+            Load(@"Assets\ship_red.png");
+        }
+    }
+}
